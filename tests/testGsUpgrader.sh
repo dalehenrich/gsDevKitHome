e4307d84cc61234c28aeed635f5e8189098a96e7
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2014-09-05 16:19:43 -0700

    adjust GsUpgrader to work on older versions of Metacello

---
+++ tests/testGsUpgrader.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+set -e # exit on error
+
+stopStone travis
+stopStone travis_2
+stoneExtent travis_2
+startStone travis_2
+
+cd ${GS_HOME}/gemstone/stones/travis_2
+
+. defStone.env
+
+topaz -l -q <<EOF
+login
+run
+Metacello new
+  baseline: 'GsUpgrader';
+  repository: 'filetree://${GS_HOME}/upgrades/repository';
+  load.
+(Smalltalk at: #GsUpgrader) upgradeGLASS.
+(Smalltalk at: #GsUpgrader) upgradeGLASS.
+(Smalltalk at: #GsUpgrader) upgradeMetacello.
+(Smalltalk at: #GsUpgrader) upgradeMetacello.
+(Smalltalk at: #GsUpgrader) upgradeGLASS1.
+(Smalltalk at: #GsUpgrader) upgradeGLASS1.
+(Smalltalk at: #GsUpgrader) upgradeMetacello.
+(Smalltalk at: #GsUpgrader) upgradeGLASS1.
+EOF
+
